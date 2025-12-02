1374b34e87aa0c5f0feb6117aca8e8b5d673b211
unknown <Sousasz>
2025-12-02 10:42:50 -0300

    :ambulance: cors error

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,2 +1,2 @@
 // Shared frontend config
-export const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
+export const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL || 'https://backend-clinica-veterinaria-v2.onrender.com';
